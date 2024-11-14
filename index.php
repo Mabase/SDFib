dda1b965c1500df81392435475f01489b7d70b54
Leonardo Genaro <84052789+Mabase@users.noreply.github.com>
2024-11-13 22:01:21 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css\styles.css">
+    <title>Página sobre a banda Pearl Jam</title>
+</head>
+<body>
+    <header>
+        <h1>Pearl Jam</h1>
+    </header>
+    <main>
+        <section id="intro">
+            <h3>Formação</h3>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A formação do Pearl Jam é uma história interessante que começou no início dos anos 90 em Seattle, uma cidade que se tornaria o epicentro do movimento grunge.
+            </p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Stone Gossard e Jeff Ament eram membros da banda de rock alternativa Mother Love Bone. Quando o vocalista da banda, Andrew Wood, faleceu em 1990, Gossard e Ament decidiram seguir em frente com um novo projeto. Mike McCready, um guitarrista que já havia tocado com Gossard e Ament em projetos anteriores, se juntou a eles.
+            </p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Eddie Vedder, que na época estava vivendo na Califórnia e havia gravado uma fita demo chamada Momma-Son, que acabou nas mãos de Gossard e Ament. Eles ficaram impressionados com a voz e o estilo de Vedder e o convidaram para se juntar à banda.
+            </p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Com a adição de Vedder, a banda começou a se formar. Inicialmente, eles eram conhecidos como Mookie Blaylock, nome inspirado no jogador de basquete. No entanto, logo decidiram mudar o nome para Pearl Jam. O nome Pearl Jam é frequentemente dito ter vindo de uma combinação de fatores, incluindo a inspiração do avô de Vedder e o desejo de ter um nome mais distintivo.
+            </p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A banda gravou suas primeiras demos e começou a tocar ao vivo em Seattle. A energia e a química entre os membros se mostraram promissoras. Em 1991, eles assinaram contrato com a Epic Records e lançaram seu álbum de estreia, Ten. Este álbum, com faixas como "Alive," "Even Flow" e "Jeremy," rapidamente ganhou popularidade e ajudou a definir o som grunge.
+            </p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Matt Cameron se juntou à banda como baterista em 1998, após a saída de Dave Abbruzzese. Cameron já era conhecido por seu trabalho com a banda Soundgarden, e sua adição trouxe uma nova dinâmica à banda.
+            </p>
+        </section>
+        <section id="integrantes-list">
+            <h2>Integrantes</h2>
+            <table>
+                <tr>
+                    <td>
+                        <img src="image/Eddie Vedder.jfif" alt="Eddie Vedder">
+                        <p><strong>Eddie Vedder</strong><br>Entrou em 1990;<br>Vocal principal e guitarrista rítmico.</p>
+                    </td>
+                    <td>
+                        <img src="image/Jeff Ament.jfif" alt="Jeff Ament">
+                        <p><strong>Jeff Ament</strong><br>Entrou em 1990;<br>Baixo e vocal de apoio.</p>
+                    </td>
+                    <td>
+                        <img src="image/Matt Cameron.jfif" alt="Matt Cameron">
+                        <p><strong>Matt Cameron</strong><br>Entrou em 1998;<br>Bateria.</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <img src="image/Mike McCready.jfif" alt="Mike McCready">
+                        <p><strong>Mike McCready</strong><br>Entrou em 1990;<br>Guitarrista líder e vocal de apoio.</p>
+                    </td>
+                    <td>
+                        <img src="image/Stone Gossard.jfif" alt="Stone Gossard">
+                        <p><strong>Stone Gossard</strong><br>Entrou em 1990;<br>Guitarrista líder e vocal de apoio.</p>
+                    </td>
+                </tr>
+            </table>
+        </section>
+        <section id="outro">
+            <h3>Importância no Grunge</h3>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;O Pearl Jam ajudou a levar o grunge, um estilo musical que surgiu em Seattle, para uma audiência global. O álbum de estreia da banda, Ten (1991), foi um grande sucesso comercial e ajudou a definir o som e a estética do grunge para o público em geral.
+            </p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A banda misturou influências do punk, do heavy metal e do rock clássico, criando um som pesado e melódico que se tornou característico do grunge. As guitarras distorcidas, a bateria potente e as letras introspectivas foram elementos centrais em seu estilo musical.
+            </p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;As letras do Pearl Jam frequentemente abordam temas de alienação, angústia e introspecção, que ressoavam profundamente com a geração que vivia no contexto social e cultural da época. Essas temáticas se tornaram uma marca registrada do grunge.
+            </p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;O Pearl Jam adotou uma postura anti-comercial e anti-establishment, alinhando-se com o ethos do grunge de rejeição à indústria musical convencional e à cultura pop superficial. A banda foi conhecida por sua resistência a vídeos musicais e à exploração comercial de sua imagem.
+            </p>
+            <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A influência do Pearl Jam vai além do grunge, moldando o rock alternativo em geral e inspirando várias bandas e músicos que vieram depois. A banda continua a ser uma força importante na música, mantendo um som que evolui enquanto permanece fiel às suas raízes grunge.
+            </p>    
+        </section>
+        <section id="Músicas mais famosas-list">
+            <h2>Musicas</h2>
+            <table>
+                <tr>
+                    <td>
+                        <img src="image/Capa do álbum Ten.jfif" alt="">
+                        <p><strong>Black</strong><br>Álbum: Ten<br>Lançado em 1991</p>
+                    </td>
+                    <td>
+                        <img src="image/Capa do álbum Ten.jfif" alt="">
+                        <p><strong>Even Flow</strong><br>Álbum: Ten<br>Lançado em 1991</p>
+                    </td>
+                    <td>
+                        <img src="image/Capa do álbum Ten.jfif" alt="">
+                        <p><strong>Alive</strong><br>Álbum: Ten<br>Lançado em 1991</p>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <img src="image/Capa do álbum Ten.jfif" alt="">
+                        <p><strong>Jeremy</strong><br>Álbum: Ten<br>Lançado em 1991</p>
+                    </td>
+                    <td>
+                        <img src="image/Last kiss.png" alt="">
+                        <p><strong>Last Kiss</strong><br>EP: Last Kiss<br>Lançado em 1999.</p>
+                    </td>
+                </tr>
+            </table>
+        </section>
+
+    </main>
+    <footer>
+        <a href="php/cadcli.php">Cadastro de Clientes</a>
+        <a href="php/cadpro.php">Cadastro de Produtos</a>
+        <a href="php/cadvenda.php">Cadastro da Venda</a>
+    </footer>
+</body>
+</html>
